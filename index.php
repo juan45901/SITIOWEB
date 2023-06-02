28aac4e8bc950d0295708001b02f6914a40bbbf3
Juan Gabriel Mususu Cubides <asistenciatics19@areandina.edu.co>
2023-06-02 13:31:01 -0500

    01

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include("template/cabecera.php"); ?>
+        <div class="jumbotron text-center">
+            <h1 class="display-3">Bienvenidos al sitio web Libros</h1>
+            <p class="lead">Consulta libros de programación</p>
+            <hr class="my-2">
+        <img width="400" src="img/Libros.PNG" class="img-thumbnail rounded mx-auto d-block"/>
+
+            <p>Más información</p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="productos.php" role="button">Ver alamcen de libros </a>
+            </p>
+        </div>
+<?php include("template/pie.php")?>
+
